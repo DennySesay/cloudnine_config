326bfa551688ceaa0bc7ccbad303f647114fe93c
Denny Sesay <dennysesay@gmail.com>
2024-12-12 02:08:47 +0100

    added enums

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+enum Plate {
+    Aluminum,
+    PolyPropelyne,
+}
+
+enum Foam {
+    Plate,
+    Case,
+}
+
+enum Mount {
+    Blue,
+    White,
+    Orange,
+    Black,
+}
+
+enum WeightAmmount {
+    One,
+    Two,
+    Three,
+    Four,
+}
+
 fn main() {
     println!("Hello, world!");
     println!("lists all config options for our beloved cloudnine");
