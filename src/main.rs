1900bcd1bee6afb99109b95ea8df4af669242b08
Sesay, Denny <denny.sesay@comline.de>
2024-12-12 10:34:01 +0100

    done

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,20 @@
-enum Plate {
-    Aluminum,
-    PolyPropelyne,
-}
-
-enum Foam {
-    Plate,
-    Case,
-}
-
-enum Mount {
-    Blue,
-    White,
-    Orange,
-    Black,
-}
-
-enum WeightAmmount {
-    One,
-    Two,
-    Three,
-    Four,
-}
-
 fn main() {
     println!("Hello, world!");
     println!("lists all config options for our beloved cloudnine");
     println!("plates, foams, tadpoles, weights");
+
+    let plates = vec!["Aluminum", "PolyPropelyne"];
+    let foams = vec!["Plate", "Case", "None"];
+    let mounts = vec!["Blue", "White", "Orange", "Black"];
+    let weights = vec!["One", "Two", "Three", "Four"];
+
+    for plate in &plates {
+        for foam in &foams {
+            for mount in &mounts {
+                for weight in &weights {
+                    println!("{}, {}, {}, {}", plate, foam, mount, weight);
+                }
+            }
+        }
+    }
 }
